e5d061091adda33648219e52b048d921be3e01ad
Michael Vigilante <vigilante.michael@gmail.com>
2016-09-03 23:41:30 +1000

    Added a whole lot of data functonality

--- src/eddy/core.clj
+++ src/eddy/core.clj
@@ -2,11 +2,23 @@
   (:gen-class)
   (:require [eddy.schedule :refer [schedule-jobs sch]]
             [eddy.schedule.calendar-interval :refer [every]]
+            [eddy.data :as data]
+            [postal.core :as postal]
+            [clj-time.core :as t]
             [clojurewerkz.quartzite.jobs :refer [defjob]]))
 
-(defjob NoOpJob [ctx]
-  (println "Something"))
+(defjob MailJob [ctx]
+  (println "sending?")
+  (postal/send-message {:from "blah@blah"
+                        :to "someone@somewhere"
+                        :subject "Something"
+                        :body "Abc"})
+  (println "sent?"))
 
 (defn -main [& args]
-  (schedule-jobs
-   (sch NoOpJob (every 1 second))))
+  (let [now (t/date-time 2016 6 2)
+        changed-objects (data/leaves-since-last-check)]
+    (doseq [x changed-objects]
+      (println (get x :key)))
+    (data/put-last-check now)
+    (println (str (data/get-last-check)))))
--- src/eddy/data.clj
+++ src/eddy/data.clj
@@ -1,6 +1,11 @@
 (ns eddy.data
-  (:require [amazonica.aws.s3 :refer [get-object list-objects]]
+  (:require [amazonica.aws.s3 :refer [get-object put-object list-objects]]
+            [clj-time.format :as time-format]
+            [environ.core :refer [env]]
             [clojure.java.io :refer [reader]]))
+
+(def zephyrus-bucket (env :zephyrus-bucket))
+(def data-bucket (env :data-bucket))
 
 (defn get-leaves [bucket]
   (filter #(re-find #"[^\/]$" (get % :key))
@@ -8,4 +13,25 @@
 
 (defn leaves-after [bucket time]
   (filter #(> 0 (compare time (get % :last-modified)))
-          (get-leaves)))
+          (get-leaves bucket)))
+
+(defn get-data [bucket path]
+  (let [s3-response (get-object bucket path)]
+    (slurp (reader (get s3-response :object-content)))))
+
+(defn put-data [bucket path data]
+  (let [bytes (.getBytes data "UTF-8")
+        input-stream (java.io.ByteArrayInputStream. bytes)]
+    (put-object :bucket-name bucket
+                :key path
+                :input-stream input-stream
+                :metadata {:content-length (count bytes)})))
+
+(defn get-last-check []
+  (time-format/parse (get-data data-bucket "last-check")))
+
+(defn put-last-check [date]
+  (put-data data-bucket "last-check" (str date)))
+
+(defn leaves-since-last-check []
+  (leaves-after zephyrus-bucket (get-last-check)))
