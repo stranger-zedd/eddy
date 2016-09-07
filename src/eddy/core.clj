25e6524c07a6a5a70babb6c8d2d761b02ff37c25
Michael Vigilante <vigilante.michael@gmail.com>
2016-09-07 21:13:31 +1000

    Added job schedule and a little extra logging

--- src/eddy/core.clj
+++ src/eddy/core.clj
@@ -5,18 +5,26 @@
             [eddy.data :as data]
             [eddy.content :as content]
             [eddy.mail :as mail]
+            [clojure.string :refer [join]]
             [clj-time.core :as t]
             [clojure.tools.logging :as log]
             [clojurewerkz.quartzite.jobs :refer [defjob]]))
 
-(defn -main [& args]
+(defjob check-and-mail [ctx]
+  (log/info "Checking for changed objects...")
   (let [now (t/now)
         changed-objects (data/leaves-since-last-check)
         email-body (content/render (data/get-email-template) changed-objects)]
     (if (not (empty? changed-objects))
-      (doseq [address (data/get-mailing-list)]
-        (log/info (str "Sending notification to " address))
-        (mail/send-message address email-body)
-        (Thread/sleep (mail/sender-delay))))
+      (do
+        (log/info (str "Found changed objects:" (join ", " changed-objects)))
+        (doseq [address (data/get-mailing-list)]
+          (log/info (str "Sending notification to " address))
+          (mail/send-message address email-body)
+          (Thread/sleep (mail/sender-delay)))))
     (log/info (str "Saving last-check: " now))
     (data/put-last-check now)))
+
+(defn -main [& args]
+  (schedule-jobs
+   (sch check-and-mail (every 1 hour))))
